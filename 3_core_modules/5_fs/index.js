51245318e18ecf681f9bcea56c7a9e3adeb50720
raphaelpavao <raphaelpavao@gmail.com>
2023-06-05 20:20:21 -0300

    aula 45

---
+++ 3_core_modules/5_fs/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const http = require('http')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    fs.readFile('mensagem.html', function(err, data){
+        res.writeHead(200,{'Contenty-Type' : 'text/html'})
+        res.write(data)
+        return res.end()
+    })   
+})
+
+server.listen(port, () =>{
+    console.log(`Servidor rodando na porta: ${port}`)
+})
